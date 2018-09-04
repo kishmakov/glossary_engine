ff0c2fd6cc04ebeb3353ba6432bf0d32e5ed7c8d
Kirill Shmakov <shmakir@yandex.ru>
2018-09-04 17:48:18 +0300

    Welcome text is updated.

--- server/server/localizations.py
+++ server/server/localizations.py
@@ -1,27 +1,63 @@
 localization_el = {
+    'loc_about': ['Καλημέρα'],
     'loc_authors_list': 'Κατάλογος συγγραφέων',
     'loc_compositions': 'Συνθέσεις',
     'loc_project_name': 'Γλωσσάριο στα ελληνικά',
     'loc_selfname': 'Ελληνικά',
-    'loc_welcome': 'Καλημέρα',
     'loc_flag': 'flag_greek.png',
 }
 
 localization_en = {
+    'loc_about': [
+        (
+            'This site is a wrapper for a repository '
+            '<a href="https://github.com/kishmakov/glossary_texts">glossary_texts</a>, '
+            'designed for simplicity of reading. In the repository I collected maximally '
+            'possible number of texts related to orthodox christianity doctrine.'
+        ),
+        (
+            'Repository is open for everyone who has interest. You can contribute to it at '
+            'will: either with '
+            '<a href="https://help.github.com/articles/about-pull-requests/">pull request</a> '
+            'or by sending the text to me directly.'
+        ),
+        (
+            'Being an editor of repositiry, I\'m responsible for texts selection. Therefor '
+            'I ask you to discuss with me propriety of new text prior to do any work. You could '
+            'reach me <a href="https://vk.com/shmakov">at vk.com</a>, '
+            '<a href="https://t.me/KirillShmakov">via telegram</a> or with e-mail: shmakir@yandex.ru.'
+        )
+    ],
     'loc_authors_list': 'List of authors',
     'loc_compositions': 'Compositions',
     'loc_project_name': 'Glossary in English',
     'loc_selfname': 'English',
-    'loc_welcome': 'Welcome',
     'loc_flag': 'flag_english.png',
 }
 
 localization_ru = {
+    'loc_about': [
+        (
+            'Сайт представляет собой удобную для чтения обёртку над репозиторием текстов '
+            '<a href="https://github.com/kishmakov/glossary_texts">glossary_texts</a>. '
+            'В данном репозитории я попытался собрать в одном месте максимально возможное '
+            'множество текстов, имеющих интерес для изучения православного вероучения.'
+        ),(
+            'Репозиторий открыт для всех интересующихся. При желании вы можете помочь его расширению, '
+            'добавив в него новые тексты: или напрямую с помощью '
+            '<a href="https://help.github.com/articles/about-pull-requests/">пулл реквеста</a>, '
+            'или отослав их мне.'
+        ),(
+            'Будучи редактором данного репозитория, я отвечаю за отбор текстов. В связи с этим прошу '
+            'перед началом какой-либо работы обговорить со мной её уместность. Связаться со мной можно '
+            '<a href="https://vk.com/shmakov">в контакте</a>, <a href="https://t.me/KirillShmakov">в телеграме</a>'
+            ' или по почте: shmakir@yandex.ru.'
+        )
+    ],
     'loc_authors_list': 'Список авторов',
     'loc_compositions': 'Сочинения',
     'loc_project_name': 'Глоссарий по-русски',
     'loc_selfname': 'Русский',
-    'loc_welcome': 'Здравствуйте',
     'loc_flag': 'flag_russian.png',
 }
 
